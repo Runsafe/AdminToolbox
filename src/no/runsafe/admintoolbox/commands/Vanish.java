f87a9ad56e75c9bacaaf27bfa4f543ddf778565b
Kruithne <kruithne@gmail.com>
2013-12-13 04:01:43 +0000

    Hook up vanish command to the handler.

--- src/no/runsafe/admintoolbox/commands/Vanish.java
+++ src/no/runsafe/admintoolbox/commands/Vanish.java
@@ -1,5 +1,6 @@
 package no.runsafe.admintoolbox.commands;
 
+import no.runsafe.admintoolbox.handlers.VanishHandler;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
 
@@ -7,14 +8,26 @@ import java.util.Map;
 
 public class Vanish extends PlayerCommand
 {
-	public Vanish()
+	public Vanish(VanishHandler handler)
 	{
 		super("vanish", "Makes you invisible to players.", "runsafe.vanish.vanish");
+		this.handler = handler;
 	}
 
 	@Override
 	public String OnExecute(IPlayer executor, Map<String, String> parameters)
 	{
-		return "Nope, sorry, nothing.";
+		if (handler.isHidden(executor))
+		{
+			handler.setHidden(executor, false);
+			return "You are now visible again.";
+		}
+		else
+		{
+			handler.setHidden(executor, true);
+			return "You are now hidden.";
+		}
 	}
+
+	private final VanishHandler handler;
 }
