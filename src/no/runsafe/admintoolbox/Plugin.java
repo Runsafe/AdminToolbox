f2fba90220b6ac8c0aa99704476877bdd6c42af3
Kruithne <kruithne@gmail.com>
2013-12-22 06:24:54 +0000

    Falling back to using VanishNoPacket for now.

--- src/no/runsafe/admintoolbox/Plugin.java
+++ src/no/runsafe/admintoolbox/Plugin.java
@@ -1,8 +1,6 @@
 package no.runsafe.admintoolbox;
 
 import no.runsafe.admintoolbox.commands.*;
-import no.runsafe.admintoolbox.handlers.Hooks;
-import no.runsafe.admintoolbox.handlers.VanishHandler;
 import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.api.log.IDebug;
 import no.runsafe.framework.features.Commands;
@@ -20,14 +18,10 @@ public class Plugin extends RunsafePlugin
 
 		Debugger = getComponent(IDebug.class);
 
-		addComponent(Hooks.class);
-		addComponent(VanishHandler.class);
-
 		addComponent(Author.class);
 		addComponent(Colour.class);
 		addComponent(GiveItem.class);
 		addComponent(RenameItem.class);
 		addComponent(Mode.class);
-		addComponent(Vanish.class);
 	}
 }
